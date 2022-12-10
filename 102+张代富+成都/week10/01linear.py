40f42d71a51904f8c03d4037c1840ae2f3ff2693
zhangdaifu67 <48009217+zhangdaifu67@users.noreply.github.com>
2022-12-10 20:09:10 +0800

    liner

--- 102+张代富+成都/week10/01linear.py
+++ 102+张代富+成都/week10/01linear.py
@@ -1,21 +1,22 @@
 import torch
+from torch.nn import Parameter
+import torch.nn.functional as f
 
 
 class Linear(torch.nn.Module):
     def __init__(self, in_features, out_features, bias=True):
         super(Linear, self).__init__()
-        self.weight = torch.nn.Parameter(torch.randn(out_features, in_features))
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.empty((out_features, in_features)))
         if bias:
-            self.bias = torch.nn.Parameter(torch.randn(out_features))
+            self.bias = Parameter(torch.empty(out_features))
 
     def forward(self, _x):
-        _x = _x.mm(self.weight)
-        if self.bias:
-            _x = _x + self.bias.expand_as(_x)
-        return _x
+        return f.linear(_x, self.weight, self.bias)
 
 
 if __name__ == '__main__':
+    x = torch.randn((2, 3))
     net = Linear(3, 2)
-    x = net.forward
-    print('11', x)
+    print(net.forward(x))
